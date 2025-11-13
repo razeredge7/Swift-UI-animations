bde01d711360e2db37bd8bceb8e9e46af1aee9f4
Kevin <ckevin2797@gmail.com>
2025-11-13 08:35:14 +0700

    Initial Commit

---
+++ animations/animationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationsApp.swift
+//  animations
+//
+//  Created by Kevin on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct animationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
